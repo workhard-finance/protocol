3d4dbda01acb747610ecbf165ebd8167961197e6
0xMayflower <0xMayflower@protonmail.com>
2021-04-24 12:23:10 +0900

    add utils

---
+++ scripts/utils/go-to-next-week.ts
@@ -0,0 +1,19 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+import { goTo } from "../test/utils/utilities";
+
+async function main() {
+  await goTo(86400 * 7 + 1);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
