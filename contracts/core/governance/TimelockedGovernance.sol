70c4da3a96243df9b41df1d5a8de8140492595db
0xMayflower <0xMayflower@protonmail.com>
2021-06-13 00:20:26 +0900

    use abicoder instead experimental pragma

--- contracts/core/governance/TimelockedGovernance.sol
+++ contracts/core/governance/TimelockedGovernance.sol
@@ -1,6 +1,6 @@
 //SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.7.0;
-pragma experimental ABIEncoderV2;
+pragma abicoder v2;
 import "@openzeppelin/contracts/access/TimelockController.sol";
 import "@openzeppelin/contracts/proxy/Initializable.sol";
 
