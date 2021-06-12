5126738b540800dd1dbecefc95c1b971aa7f247a
0xMayflower <0xMayflower@protonmail.com>
2021-06-13 00:22:38 +0900

    refactor marketplace

--- contracts/core/marketplace/Marketplace.sol
+++ contracts/core/marketplace/Marketplace.sol
@@ -1,5 +1,6 @@
 //SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.7.0;
+pragma abicoder v2;
 
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
@@ -21,32 +22,20 @@ contract Marketplace is
     ERC1155Burnable,
     IMarketplace
 {
-    struct Product {
-        address manufacturer;
-        uint256 totalSupply;
-        uint256 maxSupply;
-        uint256 price;
-        uint256 profitRate;
-        string uri;
-    }
-
     using SafeERC20 for IERC20;
     using SafeERC20 for ERC20Burnable;
     using SafeMath for uint256;
 
-    ERC20Burnable commitToken;
-
-    uint256 public taxRate = 2000; // denominator is 10,000
-
-    mapping(uint256 => Product) public override products;
-
-    uint256[] public featured;
-
     uint256 public constant RATE_DENOMINATOR = 10000;
+
+    ERC20Burnable private _commitToken;
+    uint256 private _taxRate = 2000; // denominator is 10,000
+    mapping(uint256 => Product) private _products;
+    uint256[] private _featured;
 
     modifier onlyManufacturer(uint256 id) {
         require(
-            msg.sender == products[id].manufacturer,
+            msg.sender == _products[id].manufacturer,
             "allowed only for manufacturer"
         );
         _;
@@ -59,11 +48,11 @@ contract Marketplace is
 
     function initialize(
         address _gov,
-        address _commitToken,
+        address commitToken_,
         address _dividendPool
     ) public initializer {
-        taxRate = 2000; // denominator is 10,000
-        commitToken = ERC20Burnable(_commitToken);
+        _taxRate = 2000; // denominator is 10,000
+        _commitToken = ERC20Burnable(commitToken_);
         ERC20Recoverer.initialize(_gov, new address[](0));
         Governed.initialize(_gov);
         Distributor._setup(_dividendPool);
@@ -76,27 +65,27 @@ contract Marketplace is
     ) public override nonReentrant {
         require(amount > 0, "cannot buy 0");
         // check the product is for sale
-        Product storage product = products[id];
+        Product storage product = _products[id];
         require(product.manufacturer != address(0), "Product not exists");
         uint256 stock = product.maxSupply.sub(product.totalSupply);
         require(product.maxSupply == 0 || amount <= stock, "Sold out");
         require(product.maxSupply == 0 || stock > 0, "Not for sale.");
         uint256 totalPayment = product.price.mul(amount); // SafeMath prevents overflow
         // Vision Tax
-        uint256 visionTax = totalPayment.mul(taxRate).div(RATE_DENOMINATOR);
+        uint256 visionTax = totalPayment.mul(_taxRate).div(RATE_DENOMINATOR);
         // Burn tokens
         uint256 postTax = totalPayment.sub(visionTax);
         uint256 forManufacturer =
             postTax.mul(product.profitRate).div(RATE_DENOMINATOR);
         uint256 amountToBurn = postTax.sub(forManufacturer);
-        commitToken.safeTransferFrom(msg.sender, address(this), visionTax);
-        commitToken.safeTransferFrom(
+        _commitToken.safeTransferFrom(msg.sender, address(this), visionTax);
+        _commitToken.safeTransferFrom(
             msg.sender,
             product.manufacturer,
             forManufacturer
         );
-        commitToken.burnFrom(msg.sender, amountToBurn);
-        _distribute(address(commitToken), visionTax);
+        _commitToken.burnFrom(msg.sender, amountToBurn);
+        _distribute(address(_commitToken), visionTax);
         // mint & give
         _mint(to, id, amount, "");
     }
@@ -107,7 +96,7 @@ contract Marketplace is
         uint256 price
     ) external override {
         uint256 id = uint256(keccak256(abi.encodePacked(cid, msg.sender)));
-        products[id] = Product(msg.sender, 0, 0, price, profitRate, cid);
+        _products[id] = Product(msg.sender, 0, 0, price, profitRate, cid);
         emit NewProduct(id, msg.sender, cid);
     }
 
@@ -118,7 +107,7 @@ contract Marketplace is
         uint256 maxSupply
     ) external override {
         uint256 id = uint256(keccak256(bytes(cid)));
-        products[id] = Product(
+        _products[id] = Product(
             msg.sender,
             0,
             maxSupply,
@@ -137,12 +126,12 @@ contract Marketplace is
         override
         onlyManufacturer(id)
     {
-        require(products[id].maxSupply == 0, "Max supply is already set");
+        require(_products[id].maxSupply == 0, "Max supply is already set");
         require(
-            products[id].totalSupply <= _maxSupply,
+            _products[id].totalSupply <= _maxSupply,
             "Max supply is less than current supply"
         );
-        products[id].maxSupply = _maxSupply;
+        _products[id].maxSupply = _maxSupply;
     }
 
     function setPrice(uint256 id, uint256 price)
@@ -152,7 +141,7 @@ contract Marketplace is
     {
         // to prevent overflow
         require(price * 1000000000 > price, "Cannot be expensive too much");
-        products[id].price = price;
+        _products[id].price = price;
         emit PriceUpdated(id, price);
     }
 
@@ -168,21 +157,42 @@ contract Marketplace is
         onlyManufacturer(id)
     {
         require(profitRate <= RATE_DENOMINATOR, "Profit rate is too high");
-        products[id].profitRate = profitRate;
+        _products[id].profitRate = profitRate;
         emit ProfitRateUpdated(id, profitRate);
     }
 
-    function setFeatured(uint256[] calldata _featured)
+    function setFeatured(uint256[] calldata featured_)
         external
         override
         governed
     {
-        featured = _featured;
+        _featured = featured_;
     }
 
     function setTaxRate(uint256 rate) public override governed {
         require(rate <= RATE_DENOMINATOR);
-        taxRate = rate;
+        _taxRate = rate;
+    }
+
+    function commitToken() public view override returns (address) {
+        return address(_commitToken);
+    }
+
+    function taxRate() public view override returns (uint256) {
+        return _taxRate;
+    }
+
+    function products(uint256 id)
+        public
+        view
+        override
+        returns (Product memory)
+    {
+        return _products[id];
+    }
+
+    function featured() public view override returns (uint256[] memory) {
+        return _featured;
     }
 
     function uri(uint256 id)
@@ -191,7 +201,7 @@ contract Marketplace is
         override(IERC1155MetadataURI, ERC1155)
         returns (string memory)
     {
-        return string(abi.encodePacked("ipfs://", products[id].uri));
+        return string(abi.encodePacked("ipfs://", _products[id].uri));
     }
 
     function _mint(
@@ -200,12 +210,13 @@ contract Marketplace is
         uint256 amount,
         bytes memory data
     ) internal override {
-        uint256 newSupply = products[id].totalSupply.add(amount);
+        uint256 newSupply = _products[id].totalSupply.add(amount);
         require(
-            products[id].maxSupply == 0 || newSupply <= products[id].maxSupply,
+            _products[id].maxSupply == 0 ||
+                newSupply <= _products[id].maxSupply,
             "Sold out"
         );
-        products[id].totalSupply = newSupply;
+        _products[id].totalSupply = newSupply;
         super._mint(account, id, amount, data);
     }
 
@@ -217,13 +228,13 @@ contract Marketplace is
     ) internal override {
         for (uint256 i = 0; i < ids.length; i++) {
             uint256 id = ids[i];
-            uint256 newSupply = products[id].totalSupply.add(amounts[i]);
+            uint256 newSupply = _products[id].totalSupply.add(amounts[i]);
             require(
-                products[id].maxSupply == 0 ||
-                    newSupply <= products[id].maxSupply,
+                _products[id].maxSupply == 0 ||
+                    newSupply <= _products[id].maxSupply,
                 "Sold out"
             );
-            products[id].totalSupply = newSupply;
+            _products[id].totalSupply = newSupply;
         }
         super._mintBatch(to, ids, amounts, data);
     }
--- contracts/core/marketplace/interfaces/IMarketplace.sol
+++ contracts/core/marketplace/interfaces/IMarketplace.sol
@@ -1,21 +1,19 @@
 //SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.7.0;
+pragma abicoder v2;
 
 import "@openzeppelin/contracts/token/ERC1155/IERC1155MetadataURI.sol";
 
-interface IMarketplace is IERC1155MetadataURI {
-    function products(uint256 id)
-        external
-        view
-        returns (
-            address manufacturer,
-            uint256 totalSupply,
-            uint256 maxSupply,
-            uint256 price,
-            uint256 profitRate,
-            string memory uri
-        );
+struct Product {
+    address manufacturer;
+    uint256 totalSupply;
+    uint256 maxSupply;
+    uint256 price;
+    uint256 profitRate;
+    string uri;
+}
 
+interface IMarketplace is IERC1155MetadataURI {
     function buy(
         uint256 id,
         address to,
@@ -44,6 +42,14 @@ interface IMarketplace is IERC1155MetadataURI {
     function setTaxRate(uint256 rate) external;
 
     function setFeatured(uint256[] calldata _featured) external;
+
+    function commitToken() external view returns (address);
+
+    function taxRate() external view returns (uint256);
+
+    function products(uint256 id) external view returns (Product memory);
+
+    function featured() external view returns (uint256[] memory);
 
     event NewProduct(uint256 id, address manufacturer, string uri);
 
